6741511611bd8d95a6f66ffad92d1e94ba58ef1c
Pablosornp <pablo.osorno.94@gmail.com>
2019-12-01 12:09:34 +0100

    Initial commit

---
+++ src/main/java/es/upm/pproject/sokoban/App.java
@@ -0,0 +1,13 @@
+package es.upm.pproject.sokoban;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
